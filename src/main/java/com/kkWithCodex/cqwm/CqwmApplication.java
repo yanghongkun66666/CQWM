859c1b2751df7f62dd6fd742163392d22ee2c81e
yanghongkun666666 <1355616593@qq.com>
2025-11-24 22:07:53 +0800

    first commit

---
+++ src/main/java/com/kkWithCodex/cqwm/CqwmApplication.java
@@ -0,0 +1,13 @@
+package com.kkWithCodex.cqwm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CqwmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CqwmApplication.class, args);
+    }
+
+}
